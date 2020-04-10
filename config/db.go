26542b732e37350bf6920815e2b2342ea4221df1
awilby <awilby@ucsd.edu>
2020-04-10 18:44:30 +1000

    pushing after every change is a really great way to develop

--- config/db.go
+++ config/db.go
@@ -16,17 +16,17 @@ var db *sql.DB
 func SetupDB() *sql.DB {
 
     var (
-            connectionName = "/actup-273804:us-central1:postgres-actup" //mustGetenv("CLOUDSQL_CONNECTION")
+            connectionName = "actup-273804:us-central1:postgres-actup" //mustGetenv("CLOUDSQL_CONNECTION")
             user           = "postgres" //mustGetenv("CLOUDSQL_USER")
             dbName         = "advocacy_db" //os.Getenv("CLOUDSQL_DATABASE")
             password       = "postgres" //os.Getenv("CLOUDSQL_PASSWORD")
-            socket         = "/cloudsql" //os.Getenv("CLOUDSQL_SOCKET_PREFIX")
+            socket         = "cloudsql" //os.Getenv("CLOUDSQL_SOCKET_PREFIX")
         )
 
 
 
     // connection string format: user=USER password=PASSWORD host=/cloudsql/PROJECT_ID:REGION_ID:INSTANCE_ID/[ dbname=DB_NAME]
-    dbURI := fmt.Sprintf("user=%s password=%s host=/cloudsql/%s dbname=%s", user, password, connectionName, dbName)
+    dbURI := fmt.Sprintf("user=%s password=%s host=/%s/%s dbname=%s", user, password, socket, connectionName, dbName)
     conn, err := sql.Open("postgres", dbURI)
 
     if err != nil {
