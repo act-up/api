48af8c4c0a3fc021d746db7c8fc5ad9313048bb9
awilby <awilby@ucsd.edu>
2020-04-11 21:07:27 +1000

    whoops

--- config/routes.go
+++ config/routes.go
@@ -14,12 +14,13 @@ func SetupRoutes(db *gorm.DB) *gin.Engine {
     // Declare a new router
     r := gin.Default()
 
-    r.Use(cors.Default())   // use default cors policies to allow all origins 
+    //r.Use(cors.Default())   // use default cors policies to allow all origins
 
-    /*r.Use(func(c *gin.Context) {
+    r.Use(func(c *gin.Context) {
+        cors.Default()
         c.Set("db", db)
         c.Next()
-    })*/
+    })
 
     /*r.Use(cors.New(cors.Config {
         AllowOrigins:     []string{"*"},
@@ -47,21 +48,4 @@ func SetupRoutes(db *gorm.DB) *gin.Engine {
 
     return r
 
-}
-
-
-func CORS() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, PATCH")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
 }
